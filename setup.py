853711992600915974e9b47a68b48cd42a846a78
Anton A. Lysenkov <aalyisenkov@gmail.com>
2016-03-25 18:25:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     install_requires=[
         'pillow>=2.8.1',
         'tinycss>=0.3',
-        'six>=1.10.0',
+        'six>=1.9.0',
         'requests>=2.9.1'
     ],
     extras_require={
